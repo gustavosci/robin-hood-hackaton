6390509959dd5f7dba5a7cbc9e6a70ce98f145c3
gsantosrs <gustavo.santos@reltio.com>
2023-08-22 11:37:04 +0100

    added deps

---
+++ src/main/java/com/reltio/robinhoodhackaton/RobinHoodHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.reltio.robinhoodhackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinHoodHackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinHoodHackatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reltio/robinhoodhackaton/RobinHoodHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reltio.robinhoodhackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinHoodHackatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
